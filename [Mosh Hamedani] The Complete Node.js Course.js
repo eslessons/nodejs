627c6e66a042199ba7f9cd693611712defce48dc
ES Lessons <eslessons@tutanota.com>
2021-04-26 08:41:31 +0300

    Start Mosh Hamedani - The complete Node.js Course -> 01 Getting Started

---
+++ [Mosh Hamedani] The Complete Node.js Course.js
@@ -0,0 +1,23 @@
+/* Mosh Hamedani - 
+The Complete Node.js Course
+
+01 Getting Started
+Start
+
+
+02 Node Module System
+03 Node Package Manager
+04 Building RESTful APIs Using Express
+05 Express- Advanced Topics
+06 Asynchronous JavaScript
+07 CRUD Operations Using MongoDB
+08 Mongoose - Data Validation
+09 Mongoose- Modeling Relationships Between Connected Data
+10 Authentication and Authorization
+11 Handling and Logging Errors
+12 Unit Testing
+13 Integration Testing
+14 Test-driven Development
+15 Deployment
+
+*/
