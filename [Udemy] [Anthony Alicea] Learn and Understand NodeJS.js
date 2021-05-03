286a1798a8e154d25554766938c09ccd38abcf2d
ES Lessons <eslessons@tutanota.com>
2021-05-03 10:30:52 +0300

    Start Udemy - Learn and Understand NodeJS

---
+++ [Udemy] [Anthony Alicea] Learn and Understand NodeJS.js
@@ -0,0 +1,16 @@
+/* Udemy - 
+Learn and Understand NodeJS
+
+1 - Introduction and Setup
+2 - V8 The Javascript Engine
+3 - The Node Core
+4 - Modules, Exports, and Require
+5 - Events and the Event Emitter
+6 - Asynchronous Code, libuv, The Event Loop, Streams, Files, and more
+7 - HTTP and being a Web Server
+8 - NPM the Node Package Manager
+9 - Express
+10 - Javascript, JSON, and Databases
+11 - The MEAN stack
+
+*/
