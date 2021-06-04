0a7232736488f357699cf32ed3a219ca3f79af0f
ES Lessons <eslessons@tutanota.com>
2021-06-04 12:22:12 +0300

    Start [Udemy][Jonas Schmedtmann] Node.js, Express, MongoDB & More The Complete Bootcamp

---
+++ [Udemy][Jonas Schmedtmann] Node.js, Express, MongoDB & More The Complete Bootcamp.js
@@ -0,0 +1,20 @@
+/* Udemy / Jonas Schmedtmann /
+Node.js, Express, MongoDB & More The Complete Bootcamp
+
+01 Welcome Welcome Welcome 
+02 Introduction to Node.js and NPM 
+03 Introduction to Back-End Web Development 
+04 How Node.js Works A Look Behind the Scenes 
+05 [Optional] Asynchronous JavaScript Promises and AsyncAwait 
+06 Express Lets Start Building the Natours API 
+07 Introduction to MongoDB 
+08 Using MongoDB with Mongoose 
+09 Error Handling with Express 
+10 Authentication Authorization and Security 
+11 Modelling Data and Advanced Mongoose 
+12 Server-Side Rendering with Pug Templates 
+13 Advanced Features Payments Email File Uploads 
+14 Setting Up Git and Deployment 
+15 Thats It Everyone
+
+*/
