40ce63fb393ee5b215724011f6cad503411f3afd
ES Lessons <eslessons@tutanota.com>
2021-06-25 13:30:52 +0300

    Start [Udemy][Andrew Mead] The Complete Node.js Developer Course

---
+++ [Udemy][Andrew Mead] The Complete Node.js Developer Course.js
@@ -0,0 +1,26 @@
+/* 
+[Udemy]
+[Andrew Mead] 
+The Complete Node.js 
+Developer Course
+
+01 Welcome 
+02 Installing and Exploring Node.js 
+03 Node.js Module System (Notes App) 
+04 File System and Command Line Args (Notes App) 
+05 Debugging Node.js (Notes Apps) 
+06 Asynchronous Node.js (Weather App) 
+07 Web Servers (Weather App) 
+08 Accessing API from Browser  (Weather App) 
+09 Application Deployment (Weather App) 
+10 MongoDB and Promises (Task App) 
+11 REST APIs and Mongoose (Task App) 
+12 API Authentication and Security (Task App) 
+13 Sorting, Pagination, and Filtering (Task App) 
+14 File Uploads (Task App) 
+15 Sending Emails (Task App) 
+16 Testing Node.js (Task App) 
+17 Real-Time Web Applications with Socket.io (Chat App) 
+18 Wrapping Up
+
+*/
